39a63d2196a00ec4bb4d3f295d6ad3695d992bb9
Luke Hsiao <lwhsiao@stanford.edu>
2018-11-12 09:51:47 -0800

    Capture JS and store in path relative to script

--- inspect_streams.py
+++ inspect_streams.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+import os
 
 from six.moves import range
 
@@ -10,12 +11,12 @@ from get_sites import update_last_scanned, get_urls_to_inspect
 
 
 # The list of sites that we wish to crawl
-#days_ago_3 = int(time.time()) - 86400*3
-#update_last_scanned(days_ago_3)
+# days_ago_3 = int(time.time()) - 86400*3
+# update_last_scanned(days_ago_3)
 NUM_BROWSERS = 3
 sites = get_urls_to_inspect()
 print(sites)
-#sites = ['http://www.example.com',
+# sites = ['http://www.example.com',
 #         'http://www.princeton.edu',
 #         'http://citp.princeton.edu/']
 
@@ -25,14 +26,17 @@ manager_params, browser_params = TaskManager.load_default_params(NUM_BROWSERS)
 # Update browser configuration (use this for per-browser settings)
 for i in range(NUM_BROWSERS):
     # Record HTTP Requests and Responses
-    browser_params[i]['http_instrument'] = True
+    browser_params[i]["http_instrument"] = True
     # Enable flash for all three browsers
-    browser_params[i]['disable_flash'] = False
-browser_params[0]['headless'] = True  # Launch only browser 0 headless
+    browser_params[i]["disable_flash"] = False
+    # Record js
+    browser_params[i]["js_instrument"] = True
+    browser_params[i]["headless"] = True
 
 # Update TaskManager configuration (use this for crawl-wide settings)
-manager_params['data_directory'] = '~/Desktop/'
-manager_params['log_directory'] = '~/Desktop/'
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "/data/"
+manager_params["data_directory"] = dir_path
+manager_params["log_directory"] = dir_path
 
 # Instantiates the measurement platform
 # Commands time out by default after 60 seconds
@@ -45,16 +49,22 @@ for site in sites:
     # Start by visiting the page
     command_sequence.get(sleep=0, timeout=60)
 
+    # Save screenshot
+    command_sequence.save_screenshot(str(i), timeout=60)
+
+    # Save source
+    command_sequence.dump_page_source(str(i), timeout=60)
+
     # dump_profile_cookies/dump_flash_cookies closes the current tab.
     command_sequence.dump_profile_cookies(120)
 
     # index='**' synchronizes visits between the three browsers
-    manager.execute_command_sequence(command_sequence, index='**')
+    manager.execute_command_sequence(command_sequence, index="**")
 
 # Shuts down the browsers and waits for the data to finish logging
 manager.close()
 
-#TODO: Some check that inspection was successful
+# TODO: Some check that inspection was successful
 if True:
     cur_time = int(time.time())
     update_last_scanned(cur_time)
