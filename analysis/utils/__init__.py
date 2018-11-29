68d484648b86821bdb903a205917db8eb6189440
Luke Hsiao <lwhsiao@stanford.edu>
2018-11-28 20:40:29 -0800

    Add code for generating channel provider data
    This also enhances the Alexa histogram plot by having a bucket for those
    not in the top million.

---
+++ analysis/utils/__init__.py
@@ -0,0 +1,7 @@
+from utils.database import (
+    get_channel_providers,
+    urls_per_channel_provider,
+    total_stream_urls,
+)
+
+__all__ = ["get_channel_providers", "urls_per_channel_provider", "total_stream_urls"]
