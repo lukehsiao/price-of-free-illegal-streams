bd52864f779123caea4e884c9c1b4df093a6cbb1
Luke Hsiao <lwhsiao@stanford.edu>
2018-12-03 19:53:59 -0800

    Track cookies/requests at the channel-provider level

--- analysis/tracking.py
+++ analysis/tracking.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 
-import sqlite3
 import logging
+import sqlite3
+from pprint import pprint
+from urllib.parse import urlparse
+
+from tqdm import tqdm
 
 from utils import EasyList
 
 logging.basicConfig(
     format="[%(asctime)s][%(levelname)s] %(name)s - %(message)s",
-    #  filename="tracking.log",
-    level=logging.DEBUG,
+    filename="tracking.log",
+    level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
 
 DELIMITER = "~^~"
+
+
+def get_base_url(url):
+    o = urlparse(url)
+    return o.netloc
 
 
 def get_third_parties(conn, easylist):
@@ -20,19 +29,23 @@ def get_third_parties(conn, easylist):
     third_parties = dict()
     c = conn.cursor()
 
-    for row in c.execute(
-        """
-        SELECT
-            s.visit_id,
-            s.site_url,
-            (
-                SELECT group_concat(url, '{}')
-                FROM http_requests AS r
-                WHERE s.visit_id = r.visit_id AND r.is_third_party_channel = 1
-            ) AS r_urls
-        FROM site_visits AS s;
-        """.format(
-            DELIMITER
+    for row in tqdm(
+        list(
+            c.execute(
+                """
+                SELECT
+                    s.visit_id,
+                    s.site_url,
+                    (
+                        SELECT group_concat(url, '{}')
+                        FROM http_requests AS r
+                        WHERE s.visit_id = r.visit_id AND r.is_third_party_channel = 1
+                    ) AS r_urls
+                FROM site_visits AS s;
+                """.format(
+                    DELIMITER
+                )
+            )
         )
     ):
         (visit_id, site_url, requests) = row
@@ -40,24 +53,27 @@ def get_third_parties(conn, easylist):
         if not requests:
             continue
 
+        base_url = get_base_url(site_url)
+
         requests = requests.split(DELIMITER)
 
         logger.debug("{}: {}".format(site_url, requests))
 
-        third_parties[site_url] = dict()
-        third_parties[site_url]["total_domains"] = 0
-        third_parties[site_url]["total_trackers"] = 0
+        if base_url not in third_parties:
+            third_parties[base_url] = dict()
+
+        if "total_domains" not in third_parties[base_url]:
+            third_parties[base_url]["requests"] = dict()
+            third_parties[base_url]["total_requests"] = 0
+            third_parties[base_url]["total_trackers"] = 0
 
         for request in requests:
             is_tracker = easylist.rules.should_block(request)
-            third_parties[site_url][request] = is_tracker
-            third_parties[site_url]["total_domains"] += 1
+            third_parties[base_url]["requests"][request] = is_tracker
+            third_parties[base_url]["total_requests"] += 1
             if is_tracker:
-                third_parties[site_url]["total_trackers"] += 1
+                third_parties[base_url]["total_trackers"] += 1
 
-    import pdb
-
-    pdb.set_trace()
     return third_parties
 
 
@@ -66,19 +82,23 @@ def get_cookies(conn, easylist):
     cookies = dict()
     c = conn.cursor()
 
-    for row in c.execute(
-        """
-        SELECT
-            s.visit_id,
-            s.site_url,
-            (
-                SELECT group_concat(baseDomain, '{}')
-                FROM profile_cookies AS p
-                WHERE s.visit_id = p.visit_id
-            ) AS p_cookies
-        FROM site_visits AS s;
-        """.format(
-            DELIMITER
+    for row in tqdm(
+        list(
+            c.execute(
+                """
+                SELECT
+                    s.visit_id,
+                    s.site_url,
+                    (
+                        SELECT group_concat(baseDomain, '{}')
+                        FROM profile_cookies AS p
+                        WHERE s.visit_id = p.visit_id
+                    ) AS p_cookies
+                FROM site_visits AS s;
+                """.format(
+                    DELIMITER
+                )
+            )
         )
     ):
         (visit_id, site_url, cookie_domains) = row
@@ -86,19 +106,25 @@ def get_cookies(conn, easylist):
         if not cookie_domains:
             continue
 
+        base_url = get_base_url(site_url)
+
         cookie_domains = cookie_domains.split(DELIMITER)
         logger.debug("{}: {}".format(site_url, cookie_domains))
 
-        cookies[site_url] = dict()
-        cookies[site_url]["total_domains"] = 0
-        cookies[site_url]["total_trackers"] = 0
+        if base_url not in cookies:
+            cookies[base_url] = dict()
+
+        if "total_domains" not in cookies[base_url]:
+            cookies[base_url]["domains"] = dict()
+            cookies[base_url]["total_domains"] = 0
+            cookies[base_url]["total_trackers"] = 0
 
         for cookie_domain in cookie_domains:
             is_tracker = easylist.rules.should_block(cookie_domain)
-            cookies[site_url][cookie_domain] = is_tracker
-            cookies[site_url]["total_domains"] += 1
+            cookies[base_url]["domains"][cookie_domain] = is_tracker
+            cookies[base_url]["total_domains"] += 1
             if is_tracker:
-                cookies[site_url]["total_trackers"] += 1
+                cookies[base_url]["total_trackers"] += 1
 
     return cookies
 
@@ -110,18 +136,12 @@ def main():
 
     cookies = get_cookies(conn, easylist)
 
-    requests = get_third_parties(conn, easylist)
-
-    #  import pdb
-    #
-    #  pdb.set_trace()
-
-    # For each visited stream URL:
-    #   Check associated cookies
-    #   Compare against EasyPrivacy list
-    #
+    third_parties = get_third_parties(conn, easylist)
 
     conn.close()
+
+    pprint(cookies)
+    pprint(third_parties)
 
 
 if __name__ == "__main__":
