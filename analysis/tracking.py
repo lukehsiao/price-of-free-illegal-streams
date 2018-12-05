bdc138ffc16324a529d8c48e26bc51b5ef03660a
Luke Hsiao <lwhsiao@stanford.edu>
2018-12-05 00:45:41 -0800

    Serialize to JSON for additional analysis

--- analysis/tracking.py
+++ analysis/tracking.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import logging
+import json
 import sqlite3
 from pprint import pprint
 from urllib.parse import urlparse
@@ -25,102 +26,116 @@ def get_base_url(url):
 
 def get_third_parties(easylist):
 
-    conn = sqlite3.connect(DBNAME)
-    third_parties = dict()
-    c = conn.cursor()
+    try:
+        with open("third_parties.json") as handle:
+            third_parties = json.loads(handle.read())
+            return third_parties
+    except FileNotFoundError:
+        conn = sqlite3.connect(DBNAME)
+        third_parties = dict()
+        c = conn.cursor()
 
-    for row in c.execute(
-        """
-        SELECT
-            s.visit_id,
-            s.site_url,
-            (
-                SELECT group_concat(url, '{}')
-                FROM http_requests AS r
-                WHERE s.visit_id = r.visit_id AND r.is_third_party_channel = 1
-            ) AS r_urls
-        FROM site_visits AS s;
-        """.format(
-            DELIMITER
-        )
-    ):
-        (visit_id, site_url, requests) = row
+        for row in c.execute(
+            """
+            SELECT
+                s.visit_id,
+                s.site_url,
+                (
+                    SELECT group_concat(url, '{}')
+                    FROM http_requests AS r
+                    WHERE s.visit_id = r.visit_id AND r.is_third_party_channel = 1
+                ) AS r_urls
+            FROM site_visits AS s;
+            """.format(
+                DELIMITER
+            )
+        ):
+            (visit_id, site_url, requests) = row
 
-        if not requests:
-            continue
+            if not requests:
+                continue
 
-        base_url = get_base_url(site_url)
+            base_url = get_base_url(site_url)
 
-        requests = requests.split(DELIMITER)
+            requests = requests.split(DELIMITER)
 
-        logger.debug("{}: {}".format(site_url, requests))
+            logger.debug("{}: {} requests".format(site_url, len(requests)))
 
-        if base_url not in third_parties:
-            third_parties[base_url] = dict()
+            if base_url not in third_parties:
+                third_parties[base_url] = dict()
 
-        if "total_requests" not in third_parties[base_url]:
-            #  third_parties[base_url]["requests"] = dict()
-            third_parties[base_url]["total_requests"] = 0
-            third_parties[base_url]["total_trackers"] = 0
+            if "total_requests" not in third_parties[base_url]:
+                third_parties[base_url]["requests"] = dict()
+                third_parties[base_url]["total_requests"] = 0
+                third_parties[base_url]["total_trackers"] = 0
 
-        for request in requests:
-            is_tracker = easylist.rules.should_block(request)
-            #  third_parties[base_url]["requests"][request] = is_tracker
-            third_parties[base_url]["total_requests"] += 1
-            if is_tracker:
-                third_parties[base_url]["total_trackers"] += 1
+            for request in requests:
+                is_tracker = easylist.rules.should_block(request)
+                third_parties[base_url]["requests"][request] = is_tracker
+                third_parties[base_url]["total_requests"] += 1
+                if is_tracker:
+                    third_parties[base_url]["total_trackers"] += 1
 
-    conn.close()
-    return third_parties
+        conn.close()
+        with open("third_parties.json", "w") as fp:
+            json.dump(third_parties, fp)
+        return third_parties
 
 
 def get_cookies(easylist):
 
-    conn = sqlite3.connect(DBNAME)
-    cookies = dict()
-    c = conn.cursor()
+    try:
+        with open("cookies.json") as handle:
+            cookies = json.loads(handle.read())
+            return cookies
+    except FileNotFoundError:
+        conn = sqlite3.connect(DBNAME)
+        cookies = dict()
+        c = conn.cursor()
 
-    for row in c.execute(
-        """
-        SELECT
-            s.visit_id,
-            s.site_url,
-            (
-                SELECT group_concat(baseDomain, '{}')
-                FROM profile_cookies AS p
-                WHERE s.visit_id = p.visit_id
-            ) AS p_cookies
-        FROM site_visits AS s;
-        """.format(
-            DELIMITER
-        )
-    ):
-        (visit_id, site_url, cookie_domains) = row
+        for row in c.execute(
+            """
+            SELECT
+                s.visit_id,
+                s.site_url,
+                (
+                    SELECT group_concat(baseDomain, '{}')
+                    FROM profile_cookies AS p
+                    WHERE s.visit_id = p.visit_id
+                ) AS p_cookies
+            FROM site_visits AS s;
+            """.format(
+                DELIMITER
+            )
+        ):
+            (visit_id, site_url, cookie_domains) = row
 
-        if not cookie_domains:
-            continue
+            if not cookie_domains:
+                continue
 
-        base_url = get_base_url(site_url)
+            base_url = get_base_url(site_url)
 
-        cookie_domains = cookie_domains.split(DELIMITER)
-        logger.debug("{}: {}".format(site_url, cookie_domains))
+            cookie_domains = cookie_domains.split(DELIMITER)
+            logger.debug("{}: {} cookies".format(site_url, len(cookie_domains)))
 
-        if base_url not in cookies:
-            cookies[base_url] = dict()
+            if base_url not in cookies:
+                cookies[base_url] = dict()
 
-        if "total_domains" not in cookies[base_url]:
-            #  cookies[base_url]["domains"] = dict()
-            cookies[base_url]["total_domains"] = 0
-            cookies[base_url]["total_trackers"] = 0
+            if "total_domains" not in cookies[base_url]:
+                cookies[base_url]["domains"] = dict()
+                cookies[base_url]["total_domains"] = 0
+                cookies[base_url]["total_trackers"] = 0
 
-        for cookie_domain in cookie_domains:
-            is_tracker = easylist.rules.should_block(cookie_domain)
-            #  cookies[base_url]["domains"][cookie_domain] = is_tracker
-            cookies[base_url]["total_domains"] += 1
-            if is_tracker:
-                cookies[base_url]["total_trackers"] += 1
+            for cookie_domain in cookie_domains:
+                is_tracker = easylist.rules.should_block(cookie_domain)
+                cookies[base_url]["domains"][cookie_domain] = is_tracker
+                cookies[base_url]["total_domains"] += 1
+                if is_tracker:
+                    cookies[base_url]["total_trackers"] += 1
 
-    conn.close()
+        conn.close()
+        with open("cookies.json", "w") as fp:
+            json.dump(cookies, fp)
     return cookies
 
 
@@ -154,6 +169,29 @@ def latex_third_parties(third_parties, num_rows=10):
         print("{} & {} & {} & {} \\\\".format(cp, d, t, p))
 
 
+def latex_most_common_trackers(third_parties, num_rows=10):
+    all_trackers = dict()
+    total_tracking_domains = 0
+    for key, value in third_parties.items():
+        for domain, is_tracker in value["requests"].items():
+            base_url = get_base_url(domain)
+            if base_url not in all_trackers:
+                all_trackers[base_url] = 0
+
+            all_trackers[base_url] += 1
+            total_tracking_domains += 1
+
+    top_trackers = []
+    for key, value in all_trackers.items():
+        top_trackers.append((key, value, value / total_tracking_domains))
+
+    # sort by percentage
+    top_trackers.sort(key=lambda x: x[2], reverse=True)
+
+    for d, v, p in top_trackers[:num_rows]:
+        print("{} & {} & {} \\\\".format(d, v, p))
+
+
 def main():
 
     easylist = EasyList()
@@ -163,6 +201,7 @@ def main():
 
     third_parties = get_third_parties(easylist)
     latex_third_parties(third_parties)
+    latex_most_common_trackers(third_parties)
 
 
 if __name__ == "__main__":
