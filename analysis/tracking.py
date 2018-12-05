4d74cd16dc39d75fc906c7a2a40fc19282d0a6fa
Luke Hsiao <lwhsiao@stanford.edu>
2018-12-04 21:46:07 -0800

    Fix bug in tracking analysis
    Also, remove `tqdm`, which was causing a lot of overhead.

--- analysis/tracking.py
+++ analysis/tracking.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
-
 import logging
 import sqlite3
 from pprint import pprint
 from urllib.parse import urlparse
-
-from tqdm import tqdm
 
 from utils import EasyList
 
 logging.basicConfig(
     format="[%(asctime)s][%(levelname)s] %(name)s - %(message)s",
     filename="tracking.log",
-    level=logging.INFO,
+    level=logging.DEBUG,
 )
 logger = logging.getLogger(__name__)
 
 DELIMITER = "~^~"
+
+DBNAME = "../data/crawl-data.sqlite"
 
 
 def get_base_url(url):
@@ -24,28 +23,25 @@ def get_base_url(url):
     return o.netloc
 
 
-def get_third_parties(conn, easylist):
+def get_third_parties(easylist):
 
+    conn = sqlite3.connect(DBNAME)
     third_parties = dict()
     c = conn.cursor()
 
-    for row in tqdm(
-        list(
-            c.execute(
-                """
-                SELECT
-                    s.visit_id,
-                    s.site_url,
-                    (
-                        SELECT group_concat(url, '{}')
-                        FROM http_requests AS r
-                        WHERE s.visit_id = r.visit_id AND r.is_third_party_channel = 1
-                    ) AS r_urls
-                FROM site_visits AS s;
-                """.format(
-                    DELIMITER
-                )
-            )
+    for row in c.execute(
+        """
+        SELECT
+            s.visit_id,
+            s.site_url,
+            (
+                SELECT group_concat(url, '{}')
+                FROM http_requests AS r
+                WHERE s.visit_id = r.visit_id AND r.is_third_party_channel = 1
+            ) AS r_urls
+        FROM site_visits AS s;
+        """.format(
+            DELIMITER
         )
     ):
         (visit_id, site_url, requests) = row
@@ -62,43 +58,41 @@ def get_third_parties(conn, easylist):
         if base_url not in third_parties:
             third_parties[base_url] = dict()
 
-        if "total_domains" not in third_parties[base_url]:
-            third_parties[base_url]["requests"] = dict()
+        if "total_requests" not in third_parties[base_url]:
+            #  third_parties[base_url]["requests"] = dict()
             third_parties[base_url]["total_requests"] = 0
             third_parties[base_url]["total_trackers"] = 0
 
         for request in requests:
             is_tracker = easylist.rules.should_block(request)
-            third_parties[base_url]["requests"][request] = is_tracker
+            #  third_parties[base_url]["requests"][request] = is_tracker
             third_parties[base_url]["total_requests"] += 1
             if is_tracker:
                 third_parties[base_url]["total_trackers"] += 1
 
+    conn.close()
     return third_parties
 
 
-def get_cookies(conn, easylist):
+def get_cookies(easylist):
 
+    conn = sqlite3.connect(DBNAME)
     cookies = dict()
     c = conn.cursor()
 
-    for row in tqdm(
-        list(
-            c.execute(
-                """
-                SELECT
-                    s.visit_id,
-                    s.site_url,
-                    (
-                        SELECT group_concat(baseDomain, '{}')
-                        FROM profile_cookies AS p
-                        WHERE s.visit_id = p.visit_id
-                    ) AS p_cookies
-                FROM site_visits AS s;
-                """.format(
-                    DELIMITER
-                )
-            )
+    for row in c.execute(
+        """
+        SELECT
+            s.visit_id,
+            s.site_url,
+            (
+                SELECT group_concat(baseDomain, '{}')
+                FROM profile_cookies AS p
+                WHERE s.visit_id = p.visit_id
+            ) AS p_cookies
+        FROM site_visits AS s;
+        """.format(
+            DELIMITER
         )
     ):
         (visit_id, site_url, cookie_domains) = row
@@ -115,17 +109,18 @@ def get_cookies(conn, easylist):
             cookies[base_url] = dict()
 
         if "total_domains" not in cookies[base_url]:
-            cookies[base_url]["domains"] = dict()
+            #  cookies[base_url]["domains"] = dict()
             cookies[base_url]["total_domains"] = 0
             cookies[base_url]["total_trackers"] = 0
 
         for cookie_domain in cookie_domains:
             is_tracker = easylist.rules.should_block(cookie_domain)
-            cookies[base_url]["domains"][cookie_domain] = is_tracker
+            #  cookies[base_url]["domains"][cookie_domain] = is_tracker
             cookies[base_url]["total_domains"] += 1
             if is_tracker:
                 cookies[base_url]["total_trackers"] += 1
 
+    conn.close()
     return cookies
 
 
@@ -147,10 +142,10 @@ def latex_cookies(cookies, num_rows=10):
 def latex_third_parties(third_parties, num_rows=10):
     all_cps = []
     for key, value in third_parties.items():
-        domains = value["total_domains"]
+        requests = value["total_requests"]
         trackers = value["total_trackers"]
-        percentage = trackers / domains
-        all_cps.append((key, domains, trackers, percentage))
+        percentage = trackers / requests
+        all_cps.append((key, requests, trackers, percentage))
 
     # sort by percentage
     all_cps.sort(key=lambda x: x[3], reverse=True)
@@ -162,18 +157,12 @@ def latex_third_parties(third_parties, num_rows=10):
 def main():
 
     easylist = EasyList()
-    conn = sqlite3.connect("../data/crawl-data.sqlite")
 
-    cookies = get_cookies(conn, easylist)
-    third_parties = get_third_parties(conn, easylist)
-
+    cookies = get_cookies(easylist)
     latex_cookies(cookies)
+
+    third_parties = get_third_parties(easylist)
     latex_third_parties(third_parties)
-
-    conn.close()
-
-    #  pprint(cookies)
-    #  pprint(third_parties)
 
 
 if __name__ == "__main__":
