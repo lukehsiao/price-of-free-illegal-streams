4c8d142d056cc3998ebe475e1c87a702aca808b3
Luke Hsiao <lwhsiao@stanford.edu>
2018-12-05 01:18:41 -0800

    Use the same tracking json for agg analysis

--- analysis/agg_tracking.py
+++ analysis/agg_tracking.py
@@ -6,14 +6,14 @@ from psycopg2 import IntegrityError
 GCSQL_PWD = os.environ["GCSQL_PWD"]
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
 
     third_parties = {}
     cookies = {}
-    with open('third_party_json.txt', 'r') as f:
+    with open("third_parties.json", "r") as f:
         third_parties = json.load(f)
 
-    with open('cookies_json.txt', 'r') as f:
+    with open("cookies.json", "r") as f:
         cookies = json.load(f)
 
     # Init db connection
@@ -32,7 +32,7 @@ if __name__=='__main__':
         print(key)
         requests = value["total_requests"]
         trackers = value["total_trackers"]
-        #percentage = trackers / requests
+        # percentage = trackers / requests
 
         cur.execute(get_agg_cmd, (key,))
         rows = cur.fetchall()
@@ -41,17 +41,20 @@ if __name__=='__main__':
         if aggregator not in all_aggs:
             all_aggs[aggregator] = (requests, trackers)
         else:
-            all_aggs[aggregator] = (all_aggs[aggregator][0] + requests, all_aggs[aggregator][1] + trackers)
+            all_aggs[aggregator] = (
+                all_aggs[aggregator][0] + requests,
+                all_aggs[aggregator][1] + trackers,
+            )
 
     all_aggs_list = []
     print(all_aggs)
     for key in all_aggs:
         value = all_aggs[key]
-        percentage = value[1]/value[0]
+        percentage = value[1] / value[0]
         all_aggs_list.append((key, value[0], value[1], percentage))
 
     all_aggs_list.sort(key=lambda x: x[3], reverse=True)
-    for agg, d, t, p in all_aggs_list:#[:num_rows]:
+    for agg, d, t, p in all_aggs_list:  # [:num_rows]:
         print("{} & {} & {} & {} \\\\".format(agg, d, t, p))
 
     cur.close()
