81d232c36e26fc181f557228cded67de1f2732bb
Hudson Ayers <hayers@stanford.edu>
2018-12-13 21:32:58 -0800

    agg_tracking count total visists

--- analysis/agg_tracking.py
+++ analysis/agg_tracking.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     third_parties = {}
     cookies = {}
-    with open("cache/thid_parties.json", "r") as f:
+    with open("cache/third_parties.json", "r") as f:
         third_parties = json.load(f)
 
     with open("cache/cookies.json", "r") as f:
@@ -31,21 +31,20 @@ if __name__ == "__main__":
 
     all_aggs = {}
     for key, value in third_parties.items():
-        print(key)
         requests = value["total_requests"]
         trackers = value["total_trackers"]
-        # percentage = trackers / requests
+        times_visited = value["times_visited"]
 
         cur.execute(get_agg_cmd, (key,))
         rows = cur.fetchall()
         aggregator = rows[0][0]
-        print(aggregator)
         if aggregator not in all_aggs:
-            all_aggs[aggregator] = (requests, trackers)
+            all_aggs[aggregator] = (requests, trackers, times_visited)
         else:
             all_aggs[aggregator] = (
                 all_aggs[aggregator][0] + requests,
                 all_aggs[aggregator][1] + trackers,
+                all_aggs[aggregator][2] + times_visited,
             )
 
     all_aggs_list = []
@@ -53,31 +52,31 @@ if __name__ == "__main__":
     for key in all_aggs:
         value = all_aggs[key]
         percentage = value[1] / value[0]
-        all_aggs_list.append((key, value[0], value[1], percentage))
+        trackers_per_page = value[1] / value[2]
+        all_aggs_list.append((key, value[0], value[1], percentage, trackers_per_page))
 
-    all_aggs_list.sort(key=lambda x: x[3], reverse=True)
-    print("Requests table for aggregators:")
-    for agg, d, t, p in all_aggs_list[:num_rows]:  # [:num_rows]:
-        print("{} & {} & {} & {} \\\\".format(agg, d, t, p))
+    all_aggs_list.sort(key=lambda x: x[4], reverse=True)
+    for agg, d, t, p, ttp in all_aggs_list[:num_rows]:  # [:num_rows]:
+        print("{} & {:.2f} & {:.2f} \\\\".format(agg, p, ttp))
     print()
 
     all_aggs2 = {}
     for key, value in cookies.items():
-        print(key)
         requests = value["total_domains"]
         trackers = value["total_trackers"]
+        times_visited = value["times_visited"]
         # percentage = trackers / requests
 
         cur.execute(get_agg_cmd, (key,))
         rows = cur.fetchall()
         aggregator = rows[0][0]
-        print(aggregator)
         if aggregator not in all_aggs2:
-            all_aggs2[aggregator] = (requests, trackers)
+            all_aggs2[aggregator] = (requests, trackers, times_visited)
         else:
             all_aggs2[aggregator] = (
                 all_aggs2[aggregator][0] + requests,
                 all_aggs2[aggregator][1] + trackers,
+                all_aggs2[aggregator][2] + times_visited,
             )
 
     all_aggs2_list = []
@@ -85,12 +84,13 @@ if __name__ == "__main__":
     for key in all_aggs2:
         value = all_aggs2[key]
         percentage = value[1] / value[0]
-        all_aggs2_list.append((key, value[0], value[1], percentage))
+        trackers_per_page = value[1] / value[2]
+        all_aggs2_list.append((key, value[0], value[1], percentage, times_visited))
 
-    all_aggs2_list.sort(key=lambda x: x[3], reverse=True)
+    all_aggs2_list.sort(key=lambda x: x[4], reverse=True)
     print("Cookie table for aggregators")
-    for agg, d, t, p in all_aggs2_list[:num_rows]:  # [:num_rows]:
-        print("{} & {} & {} & {} \\\\".format(agg, d, t, p))
+    for agg, d, t, p, ttp in all_aggs2_list[:num_rows]:  # [:num_rows]:
+        print("{} & {:.2f} & {:.2f} \\\\".format(agg, p, ttp))
 
     cur.close()
     conn.close()
