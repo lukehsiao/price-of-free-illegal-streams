430190b867179e6af5bc0891e8e6ac2dae49f1f8
Luke Hsiao <lwhsiao@stanford.edu>
2018-12-13 22:58:30 -0800

    refactor(agg_tracker): Minor cleanup. Should still be double checked.

--- analysis/agg_tracking.py
+++ analysis/agg_tracking.py
@@ -57,36 +57,34 @@ if __name__ == "__main__":
 
     all_aggs_list.sort(key=lambda x: x[4], reverse=True)
     print("HTTP Request Table for aggregators")
-    for agg, d, t, p, ttp in all_aggs_list[:num_rows]:  # [:num_rows]:
+    for agg, d, t, p, ttp in all_aggs_list[:num_rows]:
         print("{} & {:.2f} & {:.2f} \\\\".format(agg, p, ttp))
     print()
 
     all_aggs2 = {}
     for key, value in cookies.items():
-        requests = value["total_domains"]
+        domains = value["total_domains"]
         trackers = value["total_trackers"]
         times_visited = value["times_visited"]
-        # percentage = trackers / requests
 
         cur.execute(get_agg_cmd, (key,))
         rows = cur.fetchall()
         aggregator = rows[0][0]
         if aggregator not in all_aggs2:
-            all_aggs2[aggregator] = (requests, trackers, times_visited)
+            all_aggs2[aggregator] = (domains, trackers, times_visited)
         else:
             all_aggs2[aggregator] = (
-                all_aggs2[aggregator][0] + requests,
+                all_aggs2[aggregator][0] + domains,
                 all_aggs2[aggregator][1] + trackers,
                 all_aggs2[aggregator][2] + times_visited,
             )
 
     all_aggs2_list = []
-    # print(all_aggs2)
     for key in all_aggs2:
         value = all_aggs2[key]
         percentage = value[1] / value[0]
         trackers_per_page = value[1] / value[2]
-        all_aggs2_list.append((key, value[0], value[1], percentage, times_visited))
+        all_aggs2_list.append((key, value[0], value[1], percentage, trackers_per_page))
 
     all_aggs2_list.sort(key=lambda x: x[4], reverse=True)
     print("Cookie table for aggregators")
